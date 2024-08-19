aca26a486aaf9942eea9a312661416fca72d3a6e
wiseah <hyunah2765@gmail.com>
2024-08-20 05:27:02 +0900

    MongoDB와 Express 연결

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require("mongoose")
 const app = express();
 const port = 8000;
 
+
 app.use(express.static(path.join(__dirname, '../client/build')));
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  mongoose.connect(
+    "mongodb+srv://hyunah2765:h27652765@mern-stack-study-cluste.49uhp.mongodb.net/?retryWrites=true&w=majority&appName=mern-stack-study-cluste"
+  ).then(()=> {
+    console.log(`Example app listening at http://localhost:${port}`);
+    console.log("Connecting MongoDB...")
+  }).catch((err)=> {
+    console.log(`${err}`)
+  });
+  
 });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'))
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
 });
